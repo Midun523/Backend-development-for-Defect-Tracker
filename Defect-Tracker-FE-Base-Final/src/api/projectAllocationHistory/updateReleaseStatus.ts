023d1760ae3e6c266700cc97545f6de062a01927
Midun523 <neuralnets523@gmail.com>
2026-09-18 15:39:46 +0530

    Fix updateReleaseStatus API wrapper to send JSON payload

--- Defect-Tracker-FE-Base-Final/src/api/projectAllocationHistory/updateReleaseStatus.ts
+++ Defect-Tracker-FE-Base-Final/src/api/projectAllocationHistory/updateReleaseStatus.ts
@@ -3,7 +3,8 @@ import { ENDPOINTS } from "../../utils/apiendpoint";
 
 export const updateReleaseStatus = async (releaseId: number, data: any) => {
   const token = localStorage.getItem("authToken");
-  const res = await axios.patch(ENDPOINTS.releaseStatus(releaseId), data, {
+  const payload = typeof data === 'string' ? { status: data } : data;
+  const res = await axios.patch(ENDPOINTS.releaseStatus(releaseId), payload, {
     headers: token ? { Authorization: `Bearer ${token}` } : {},
   });
   return res.data;
